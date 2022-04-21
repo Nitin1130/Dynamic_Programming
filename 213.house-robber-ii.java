ac9970ef1a76b78283ff1068279bcb1e18a45ba9
Nitin Choudhary <nitinchoudhary260@gmail.com>
2022-04-21 17:38:17 +0530

    Added robber 2

--- 213.house-robber-ii.java
+++ 213.house-robber-ii.java
@@ -7,7 +7,22 @@
 // @lc code=start
 class Solution {
     public int rob(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        if(nums.length == 2) return Math.max(nums[0], nums[1]);
         
+        int n = nums.length;
+        return Math.max(rob(nums, 0, n - 2), rob(nums, 1, n - 1));
+
+    }
+    public int rob(int[] nums, int i, int j) {
+        int dp[] = new int[nums.length];
+        dp[i] = nums[i];
+        dp[i + 1] = Math.max(nums[i], nums[i+1]);
+        for(int k = i + 2; k <= j; k++) {
+            dp[k] = Math.max(dp[k - 1], nums[k] + dp[k - 2]); 
+
+        }
+        return dp[j];
     }
 }
 // @lc code=end
