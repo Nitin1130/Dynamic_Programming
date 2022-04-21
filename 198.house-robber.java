13107394532a1fa881244fbb45275d157a7f40d3
Nitin Choudhary <nitinchoudhary260@gmail.com>
2022-04-21 16:30:42 +0530

    Dynamic Programming

---
+++ 198.house-robber.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=198 lang=java
+ *
+ * [198] House Robber
+ */
+
+// @lc code=start
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        int v1 = nums[0], v2 = Math.max(v1, nums[1]);
+        for(int i = 2; i < n; i++) {
+            int tmp = v2;
+            v2 = Math.max(v2, v1 + nums[i]);
+            v1 = tmp;
+        }
+        return v2;
+    }
+}
+// @lc code=end
+
