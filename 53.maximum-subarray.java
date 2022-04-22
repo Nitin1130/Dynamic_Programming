1205de4eb68ff97427d23a4d372f8387e25b8b83
Nitin Choudhary <nitinchoudhary260@gmail.com>
2022-04-22 11:19:38 +0530

    Added max subarray sum problems

---
+++ 53.maximum-subarray.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=53 lang=java
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max_so_far = nums[0];
+        int curr_max = nums[0];
+ 
+   for (int i = 1; i < nums.length; i++) 
+   {
+        curr_max = Math.max(nums[i], curr_max+nums[i]);
+        max_so_far = Math.max(max_so_far, curr_max);
+   }
+   return max_so_far;
+    }
+}
+// @lc code=end
+
